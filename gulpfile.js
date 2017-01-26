90e87d3efa44da81b2b6495fc7493654627f5425
ptlergo <ptlergo@gmail.com>
2017-01-26 14:54:40 -0500

    eslint rule for arrow-body-style fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
 // Static Server
 gulp.task('run', () => {
@@ -8,4 +9,12 @@ gulp.task('run', () => {
       baseDir: './client',
     },
   });
+});
+
+// Sass Compile Once
+gulp.task('sass', () => {
+  return gulp.src('')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
 });
