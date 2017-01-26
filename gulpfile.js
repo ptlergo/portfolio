b168896ec4d553decbad2c97874b40300cc29cab
ptlergo <ptlergo@gmail.com>
2017-01-26 14:37:18 -0500

    gulp run for browser sync static server. SUCCESS

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+
+// Static Server
+gulp.task('run', () => {
+  browserSync.init({
+    server: {
+      baseDir: './client',
+    },
+  });
+});
