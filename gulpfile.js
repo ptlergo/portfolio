c55c33204227b7b6cbf181ee3d17e384467cb6f1
ptlergo <ptlergo@gmail.com>
2017-01-28 03:23:29 -0500

    sass task updated. SUCCESS

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,21 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
-// Static Server
-gulp.task('run', () => {
+// Static Server + watch sass changes
+gulp.task('run', ['sass'], () => {
   browserSync.init({
     server: {
       baseDir: './client',
     },
   });
+
+  gulp.watch('./client/stylesheets/sass/*.scss');
+  gulp.watch('./client/*.html').on('change', browserSync.reload);
 });
 
 // Sass Compile Once
 gulp.task('sass', () => {
-  return gulp.src('')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.stream());
+  return gulp.src('client/stylesheets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./client/stylesheets/css'));
 });
