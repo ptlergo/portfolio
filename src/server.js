ec38772d0b87bb71812ffd3b58e99a834d819b0c
ptlergo <ptlergo@gmail.com>
2017-01-29 05:37:57 -0500

    setting headers for access

--- src/server.js
+++ src/server.js
@@ -6,3 +6,10 @@ const express = require('express'),
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authrization');
+  next();
+});
