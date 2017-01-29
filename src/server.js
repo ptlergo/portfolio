18652abc558acdeb8326a1e1ff8ae570936672c9
ptlergo <ptlergo@gmail.com>
2017-01-29 05:45:22 -0500

    server file complete. new eslint rule

--- src/server.js
+++ src/server.js
@@ -1,15 +1,32 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
   path = require('path'),
+  morgan = require('morgan'),
   app = express(),
   port = 8080;
 
+// Request stream formating
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Set headers to be able to conduct karma tests
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authrization');
   next();
 });
+
+// Logger styling
+app.use(morgan('dev'));
+
+// Front end directory path
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+
+app.listen(port);
+console.log('LISTENING TO PORT: ', port);
