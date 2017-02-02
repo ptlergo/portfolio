c0b82b1f91208ac200f169f6c46636e9e3fa91b6
ptlergo <ptlergo@gmail.com>
2017-02-02 13:22:31 -0500

    stateprovider home state defined

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,11 @@
-angular.module('myApp', ['ui.router']);
+angular.module('myApp', ['ui.router'])
+  .config(($stateProvider) => {
+    const homeState = {
+      url: '/home',
+      name: 'home',
+      template: 'this',
+      controller: 'MainController',
+
+    };
+    $stateProvider.state(homeState);
+  });
