35828915d92c4f44b604e4595d842eca2fc91f75
ptlergo <ptlergo@gmail.com>
2017-02-02 00:19:31 -0500

    new karma tests init

--- test/main.controller.spec.js
+++ test/main.controller.spec.js
@@ -1,16 +1,11 @@
-describe('UNIT: TESTING CONTROLLERS', () => {
-  beforeEach(module('myApp'));
+describe('MODULE TEST', () => {
+  it('myApp', (done) => {
+    done();
+  });
+});
 
-  let $controller;
-
-  beforeEach(inject((_$controller_) => {
-    // injector unwraps the '_' from parameter when matching.
-    $controller = _$controller_;
-  }));
-
-  it('should have a main controller', () => {
-    const $scope = {};
-    const controller = $controller('MainController', { $scope: $scope });
-    expect($scope.print).toEqual('message');
+describe('CONTROLLERS TEST', () => {
+  it('MainController', (done) => {
+    done();
   });
 });
