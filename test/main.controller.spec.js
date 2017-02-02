263c8edd714fb389a5221a61018b222b4686191f
ptlergo <ptlergo@gmail.com>
2017-02-02 00:25:12 -0500

    karma test module. SUCCESS

--- test/main.controller.spec.js
+++ test/main.controller.spec.js
@@ -1,9 +1,17 @@
+// Test all modules: currently 1 module: myApp
 describe('MODULE TEST', () => {
-  it('myApp', (done) => {
-    done();
+  let module;
+
+  beforeEach(() => {
+    module = angular.module('myApp');
+  });
+
+  it('myApp should be registered', () => {
+    expect(module).toBeDefined();
   });
 });
 
+// Test All Controllers. Currently 1: MainController
 describe('CONTROLLERS TEST', () => {
   it('MainController', (done) => {
     done();
