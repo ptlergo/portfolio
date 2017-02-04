7841386837c56691959f8277da5557eb5867f9d0
ptlergo <ptlergo@gmail.com>
2017-02-04 01:56:50 -0500

    all routes configed

--- client/js/app.js
+++ client/js/app.js
@@ -5,6 +5,24 @@ angular.module('myApp', ['ngRoute'])
       templateUrl: 'views/home.html',
     };
 
+    const aboutRoute = {
+      controller: 'MainController',
+      templateUrl: 'views/about.html',
+    };
+
+    const writingsRoute = {
+      controller: 'MainController',
+      templateUrl: 'views/writings.html',
+    };
+
+    const worksRoute = {
+      controller: 'MainController',
+      templateUrl: 'views/works.html',
+    };
+
     $routeProvider.when('/', homeRoute);
+    $routeProvider.when('/about', aboutRoute);
+    $routeProvider.when('/writings', writingsRoute);
+    $routeProvider.when('/works', worksRoute);
     $routeProvider.otherwise({ redirectTo: '/' });
   });
