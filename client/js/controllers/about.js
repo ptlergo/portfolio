d8399de1336a82ec8d696d700d1ff17ae5583c95
ptlergo <ptlergo@gmail.com>
2017-02-06 01:31:45 -0500

    content in about controller

--- client/js/controllers/about.js
+++ client/js/controllers/about.js
@@ -10,6 +10,9 @@
 function AboutController() {
   const vm = this;
   vm.printAbout = 'About View message';
+  vm.summary = 'My name is Patrick Tunga-Lergo. I am a delvoper.' +
+               'A full sail student. I write for publications' +
+               'I also do a lot of other stuff i will get into later';
 }
 
 angular.module('myApp').controller('AboutController', AboutController);
