14958b80e58f44ac3df4cbf1db50a7765981201a
ptlergo <ptlergo@gmail.com>
2017-02-07 18:42:30 -0500

    summary messages for homepage filler.

--- client/js/controllers/about.js
+++ client/js/controllers/about.js
@@ -9,10 +9,23 @@
 */
 function AboutController() {
   const vm = this;
-  vm.printAbout = 'About View message';
-  vm.summary = 'My name is Patrick Tunga-Lergo. I am a delvoper.' +
-               'A full sail student. I write for publications' +
-               'I also do a lot of other stuff i will get into later';
+  vm.printAbout = 'About Me';
+  vm.summary = `Wikipedia is a live collaboration differing from paper-based
+                reference sources in important ways. Unlike printed encyclopedias,
+                Wikipedia is continually created and updated, with articles on
+                historic events appearing within minutes, rather than months
+                or years. Because everybody can help improve it, Wikipedia
+                has become more comprehensive than any other encyclopedia.
+                In addition to quantity, its contributors work on improving
+                quality as well. Wikipedia is a work-in-progress, with articles
+                in various stages of completion. As articles develop, they tend
+                to become more comprehensive and balanced. Quality also improves
+                over time as misinformation and other errors are removed or repaired.
+                However, because anyone can click "edit" at any time and add stuff in,
+                any article may contain undetected misinformation, errors, or vandalism.
+                Awareness of this helps the reader to obtain valid information, avoid
+                recently added misinformation (see Wikipedia:Researching with Wikipedia),
+                and fix the article.`;
 }
 
 angular.module('myApp').controller('AboutController', AboutController);
--- client/js/controllers/works.js
+++ client/js/controllers/works.js
@@ -9,7 +9,23 @@
 */
 function WorksController() {
   const vm = this;
-  vm.printWorks = 'Works View message';
+  vm.printWorks = 'My Works';
+  vm.summary = `Wikipedia is a live collaboration differing from paper-based
+                reference sources in important ways. Unlike printed encyclopedias,
+                Wikipedia is continually created and updated, with articles on
+                historic events appearing within minutes, rather than months
+                or years. Because everybody can help improve it, Wikipedia
+                has become more comprehensive than any other encyclopedia.
+                In addition to quantity, its contributors work on improving
+                quality as well. Wikipedia is a work-in-progress, with articles
+                in various stages of completion. As articles develop, they tend
+                to become more comprehensive and balanced. Quality also improves
+                over time as misinformation and other errors are removed or repaired.
+                However, because anyone can click "edit" at any time and add stuff in,
+                any article may contain undetected misinformation, errors, or vandalism.
+                Awareness of this helps the reader to obtain valid information, avoid
+                recently added misinformation (see Wikipedia:Researching with Wikipedia),
+                and fix the article.`;
 }
 
 angular.module('myApp').controller('WorksController', WorksController);
