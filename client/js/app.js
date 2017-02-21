d017b1cc770dac2900369a4a822560760d308384
ptlergo <ptlergo@gmail.com>
2017-02-21 17:04:00 -0500

    inject uibootstrap for carousel. remove unneeded imports

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ngRoute'])
+angular.module('myApp', ['ngRoute', 'ui.bootstrap'])
   .config(($routeProvider, $locationProvider) => {
     const homeRoute = {
       controller: 'HomeController',
