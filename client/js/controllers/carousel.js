1844c4a9de1fb7e25912b2202548e0b1d8b2ee14
ptlergo <ptlergo@gmail.com>
2017-02-21 16:22:46 -0500

    controller array images

--- client/js/controllers/carousel.js
+++ client/js/controllers/carousel.js
@@ -1,5 +1,18 @@
 function carouselController() {
   const vm = this;
-  vm.msg = 'connected';
+  vm.imgArray = [
+    {
+      src: 'https://unsplash.com/collections/curated/133?photo=uZxT3njDQYc',
+    },
+    {
+      src: 'https://unsplash.com/collections/curated/133?photo=6xh7H5tWj9c',
+    },
+    {
+      src: 'https://unsplash.com/collections/curated/133?photo=OWbH9a8Yi2I',
+    },
+    {
+      src: 'https://unsplash.com/collections/curated/133?photo=duiETcZN7O4',
+    },
+  ];
 }
 angular.module('myApp').controller('carouselController', carouselController);
--- client/js/directives/carousel.directive.js
+++ client/js/directives/carousel.directive.js
@@ -8,21 +8,6 @@
   * @returns {}
 */
 function carousel() {
-  const vm = this;
-  vm.imgArray = [
-    {
-      src: 'https://unsplash.com/collections/curated/133?photo=uZxT3njDQYc',
-    },
-    {
-      src: 'https://unsplash.com/collections/curated/133?photo=6xh7H5tWj9c',
-    },
-    {
-      src: 'https://unsplash.com/collections/curated/133?photo=OWbH9a8Yi2I',
-    },
-    {
-      src: 'https://unsplash.com/collections/curated/133?photo=duiETcZN7O4',
-    },
-  ];
   return {
     restrict: 'AEC',
     templateUrl: 'views/templates/carousel-directive.html',
