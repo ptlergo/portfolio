bd4dc9c36b85d51f6bb371e404c6bd41e718ba68
ptlergo <ptlergo@gmail.com>
2017-02-21 18:19:38 -0500

    attempting to use title scope for carousel directive

--- client/js/controllers/carousel.js
+++ client/js/controllers/carousel.js
@@ -1,10 +1,21 @@
 function CarouselController() {
   const vm = this;
   vm.msg = 'carousel';
-  vm.slides = [
+  vm.writings = [
     {
       id: 0,
       src: 'http://i.imgur.com/H981AN7.jpg',
+    },
+    {
+      id: 1,
+      src: 'http://i.imgur.com/ArRiBMs.png',
+    },
+  ];
+
+  vm.works = [
+    {
+      id: 0,
+      src: 'http://i.imgur.com/Ls0YKZF.jpg',
     },
     {
       id: 1,
