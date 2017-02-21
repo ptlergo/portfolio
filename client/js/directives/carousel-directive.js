0d103cc59dab623b4d3e6cb63d57041d63bc72f0
ptlergo <ptlergo@gmail.com>
2017-02-21 15:20:18 -0500

    image array for carousel directive

--- client/js/directives/carousel-directive.js
+++ client/js/directives/carousel-directive.js
@@ -8,6 +8,21 @@
   * @returns {}
 */
 function Carousel() {
+  const vm = this;
+  vm.imgArray = [
+    {
+      src: 'https://unsplash.com/collections/curated/133?photo=uZxT3njDQYc',
+    },
+    {
+      src: 'https://unsplash.com/collections/curated/133?photo=6xh7H5tWj9c',
+    },
+    {
+      src: 'https://unsplash.com/collections/curated/133?photo=OWbH9a8Yi2I',
+    },
+    {
+      src: 'https://unsplash.com/collections/curated/133?photo=duiETcZN7O4',
+    },
+  ];
   return {
     msg: 'Carousel',
   };
