035717f7bc0fe4950d6956996a021e4615204ac0
ptlergo <ptlergo@gmail.com>
2017-03-12 16:37:57 -0400

    works page init

--- client/js/controllers/works.js
+++ client/js/controllers/works.js
@@ -14,6 +14,11 @@ function WorksController() {
   vm.summary = `My years have been spent creating a range of digital content such as websites
   apps, web tools, logos, and graphics. My works always begin with the question of 'how do I best
   embody my client's brand?`;
+  vm.printWorks = 'Musicem';
+  vm.printWorks = 'wintersoulstice';
+  vm.printWorks = '8Pounds';
+  vm.printWorks = 'Web Tools';
+  vm.printWorks = 'Graphics';
 }
 
 angular.module('myApp').controller('WorksController', WorksController);
