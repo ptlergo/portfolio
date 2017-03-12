3d585fc7c1fd1038bd636a2a41b8fdee9d223a6d
ptlergo <ptlergo@gmail.com>
2017-03-12 18:33:04 -0400

    works section title homepage fix and links added

--- client/js/controllers/works.js
+++ client/js/controllers/works.js
@@ -11,6 +11,7 @@ function WorksController() {
   const vm = this;
   vm.img_src = 'https://marypomerantzadvertising.com/wp-content/uploads/responsiveweb.gif';
   vm.tagline = 'Details of some projects i\'ve worked on';
+  vm.printWorks = 'My Works';
   vm.summary = `My years have been spent creating a range of digital content such as websites
   apps, web tools, logos, and graphics. I use a minimalist approach and my philosophy is 'complexity from simplicity'.
   My works always begin with the question of 'how do I efficiently embody a creative expression of my client's brand?`;
