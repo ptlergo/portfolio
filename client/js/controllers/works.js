4c8a54792a7451428d0bfdd00d0090ab77a6b85f
ptlergo <ptlergo@gmail.com>
2017-03-14 15:52:40 -0400

    use service for works controller. SUCCESS

--- client/js/controllers/works.js
+++ client/js/controllers/works.js
@@ -7,7 +7,7 @@
   *
   * @returns {}
 */
-function WorksController() {
+function WorksController(worksService) {
   const vm = this;
   vm.img_src = 'https://marypomerantzadvertising.com/wp-content/uploads/responsiveweb.gif';
   vm.tagline = 'Details of some projects i\'ve worked on';
@@ -15,68 +15,7 @@ function WorksController() {
   vm.summary = `My years have been spent creating a range of digital content such as websites
   apps, web tools, logos, and graphics. I use a minimalist approach and my philosophy is 'complexity from simplicity'.
   My works always begin with the question of 'how do I efficiently embody a creative expression of my client's brand?`;
-  vm.cases = [
-    {
-      id: 1,
-      name: 'music\'em',
-      details: 'Web app that aggregates content from curated music brands.',
-      challenges: '',
-      image: 'http://marvelapp.com/1f3ia7a?emb=1',
-      type: 'app',
-      tech: 'AngularJS, API, NodeJS, express, firebase, css3, material design, google cloud',
-      tags: '',
-    },
-    {
-      id: 2,
-      name: 'winter-soulstice',
-      details: 'web app for concert that reserves tickets and as details of performers. ',
-      challenges: '',
-      image: '../img/winter.gif',
-      type: 'app',
-      tech: 'codeIgniter, PHP, MongoDB, BootStrap3, JQuery, Heroku',
-      tags: '',
-    },
-    {
-      id: 3,
-      name: 'debug-squasher',
-      details: 'programming tool published to npm to enhance development.',
-      challenges: '',
-      image: '',
-      type: 'tool',
-      tech: 'NodeJS, javascript, NPM',
-      tags: '',
-    },
-    {
-      id: 4,
-      name: 'hamburger icon',
-      details: 'angular menu icon with animation.',
-      challenges: '',
-      image: '',
-      tech: 'AngularJS, angular-animate, css3',
-      type: 'tool',
-      tags: '',
-    },
-    {
-      id: 5,
-      name: '8Pounds Music',
-      details: 'WordPress website template with custom css and animations applied.',
-      challenges: '',
-      image: '',
-      type: 'website',
-      tech: 'PHP, css3, JQuery, WordPress',
-      tags: '',
-    },
-    {
-      id: 6,
-      name: 'MY Portfolio Page',
-      details: 'Using angular and css technigues to manipulate svgs and create animations to showcase works and expereince as a developer.',
-      challenges: '',
-      image: '',
-      type: 'web app',
-      tech: 'AngularJS, css3, JQuery, JavaScript',
-      tags: '',
-    },
-  ];
+  vm.cases = worksService.caseStudies;
 }
-
+WorksController.$inject = ['worksService'];
 angular.module('myApp').controller('WorksController', WorksController);
--- client/js/services/works.service.js
+++ client/js/services/works.service.js
@@ -8,7 +8,70 @@
   * @returns {}
 */
 function worksService() {
-
+  return {
+    caseStudies: [
+      {
+        id: 1,
+        name: 'music\'em',
+        details: 'Web app that aggregates content from curated music brands.',
+        challenges: '',
+        image: 'http://marvelapp.com/1f3ia7a?emb=1',
+        type: 'app',
+        tech: 'AngularJS, API, NodeJS, express, firebase, css3, material design, google cloud',
+        tags: '',
+      },
+      {
+        id: 2,
+        name: 'winter-soulstice',
+        details: 'web app for concert that reserves tickets and as details of performers. ',
+        challenges: '',
+        image: '../img/winter.gif',
+        type: 'app',
+        tech: 'codeIgniter, PHP, MongoDB, BootStrap3, JQuery, Heroku',
+        tags: '',
+      },
+      {
+        id: 3,
+        name: 'debug-squasher',
+        details: 'programming tool published to npm to enhance development.',
+        challenges: '',
+        image: '',
+        type: 'tool',
+        tech: 'NodeJS, javascript, NPM',
+        tags: '',
+      },
+      {
+        id: 4,
+        name: 'hamburger icon',
+        details: 'angular menu icon with animation.',
+        challenges: '',
+        image: '',
+        tech: 'AngularJS, angular-animate, css3',
+        type: 'tool',
+        tags: '',
+      },
+      {
+        id: 5,
+        name: '8Pounds Music',
+        details: 'WordPress website template with custom css and animations applied.',
+        challenges: '',
+        image: '',
+        type: 'website',
+        tech: 'PHP, css3, JQuery, WordPress',
+        tags: '',
+      },
+      {
+        id: 6,
+        name: 'MY Portfolio Page',
+        details: 'Using angular and css technigues to manipulate svgs and create animations to showcase works and expereince as a developer.',
+        challenges: '',
+        image: '',
+        type: 'web app',
+        tech: 'AngularJS, css3, JQuery, JavaScript',
+        tags: '',
+      },
+    ],
+  };
 }
 
 angular.module('myApp').factory('worksService', worksService);
