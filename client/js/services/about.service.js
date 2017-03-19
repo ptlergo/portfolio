5d8d1eb6942b1fb9d704995d24b7393754b09adc
ptlergo <ptlergo@gmail.com>
2017-03-19 00:03:03 -0400

    about service touch

---
+++ client/js/services/about.service.js
@@ -0,0 +1,15 @@
+/**
+  * @ngdoc method
+  * @name aboutService
+  * @methodOf module.aboutService
+  * @description
+  * About Page service with list of skills and expertise
+  *
+  * @returns {}
+*/
+function aboutService() {
+  return { }
+
+}
+
+angular.module('myApp').factory('aboutService', aboutService);
