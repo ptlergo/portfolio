6e799c4547149ab655450cd8779a1050e1c6581a
ptlergo <ptlergo@gmail.com>
2017-03-19 02:43:50 -0400

    restructure object properly

--- client/js/services/about.service.js
+++ client/js/services/about.service.js
@@ -1,45 +1,29 @@
 /**
-  * @ngdoc method
-  * @name aboutService
-  * @methodOf module.aboutService
-  * @description
-  * About Page service with list of skills and expertise
-  *
-  * @returns {}
-*/
+ * @ngdoc method
+ * @name aboutService
+ * @methodOf module.aboutService
+ * @description
+ * About Page service with list of skills and expertise
+ *
+ * @returns {}
+ */
 function aboutService() {
-  return {
-    'development': {
-      'name': 'Development',
-      'icon': 'k',
-      'techniques': {
-        'name': 'Techniques',
-        'icon': '',
-        'list': [],
-      },
-      'skills': {
-        'name': 'Skills',
-        'icon': '',
-        'list': ['html5', 'css3', 'angularJS', 'JavaScript/ES6',
-         'JQuery', 'Git Hooks', 'Gulp/Grunt', 'NodeJS',
-         'Sass/Less', 'React', 'ReactNative', 'ionic', ],
-      },
-    },
-    'design': {
-      'name': 'Design',
-      'icon': '',
-      'techniques': {
-        'name': 'Techniques',
-        'icon': '',
-        'list': [],
-      },
-      'tools': {
-        'name': 'Tools',
-        'icon': '',
-        'list': [],
-      },
-    },
-   }
+    return {
+        'Proficiencies': {
+            'development': {
+                'name': 'Development',
+                'icon': 'd',
+                'techniques': {
+                  'name': 'Techniques',
+                  'list': ['tech', 'd'],
+                },
+            },
+            'design': {
+                'name': 'Design',
+                'icon': 'a',
+            }
+        },
+    }
 }
 
 angular.module('myApp').factory('aboutService', aboutService);
