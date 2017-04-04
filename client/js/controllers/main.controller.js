b73d51d7d48e31ee14c3bdc13f8e90a5e77d5552
ptlergo <ptlergo@gmail.com>
2017-04-04 19:20:38 -0400

    templates in maincontroller for ngrepeat in home

--- client/js/controllers/main.controller.js
+++ client/js/controllers/main.controller.js
@@ -10,6 +10,13 @@
 function MainController() {
   const vm = this;
   vm.main_logo = '../../img/pat-logo.svg';
+
+  vm.templates = [
+    { name: 'about', url: 'views/home/about-section.html' },
+    { name: 'works', url: 'views/home/works-section.html' },
+    { name: 'publications', url: 'views/home/publications-section.html' },
+  ];
 }
+
 
 angular.module('myApp').controller('MainController', MainController);
