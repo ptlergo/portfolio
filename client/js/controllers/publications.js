c3490204b633862d1b2ec23ddbabe03cfbdbc368
ptlergo <ptlergo@gmail.com>
2017-04-04 02:13:59 -0400

    publications update summary

--- client/js/controllers/publications.js
+++ client/js/controllers/publications.js
@@ -12,8 +12,8 @@ function PublicationsController() {
   vm.img_src = 'https://marypomerantzadvertising.com/wp-content/uploads/responsiveweb.gif';
   vm.printWritings = 'My Publications';
   vm.summary = `From writing code to writing literature, my passions are intertwined. Some
-  of my writings include brand identies such as taglines, artists bios, and ad copy. I also write
-  articles on music and tech which are published online.`;
+                of my writings are for brand identies such as taglines, artists bios,
+                and advertisement copy. I also write articles on my own publication, 8Pounds Music,
+                and blog about tech on Medium during my free time.`;
 }
-
 angular.module('myApp').controller('PublicationsController', PublicationsController);
