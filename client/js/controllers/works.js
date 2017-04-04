6252ee99218fb19603e634b02564580f392e2c75
ptlergo <ptlergo@gmail.com>
2017-04-04 02:07:58 -0400

    works summary update

--- client/js/controllers/works.js
+++ client/js/controllers/works.js
@@ -12,9 +12,11 @@ function WorksController(worksService) {
   vm.img_src = '../../img/iphone.png';
   vm.tagline = 'Details of some projects i\'ve worked on';
   vm.printWorks = 'My Works';
-  vm.summary = `My years have been spent creating a range of digital content such as websites
-  apps, web tools, logos, and graphics. I use a minimalist approach and my philosophy is 'complexity from simplicity'.
-  My works always begin with the question of 'how do I effectively embody a creative expression of my client's brand?`;
+  vm.summary = `My years have been spent creating a variety of digital content as a Full Sail University
+                Web Design & Development student, and for clients such as: musicians, agencies, and companies.
+                I've made websites, apps, web tools, logos, and graphics. I believe that simplicity can
+                come from complexity which shows in my minimalist design approach. Before beginning I always 
+                ask 'how can I effectively embody the creative expression of my client's brand?`;
   vm.cases = worksService.caseStudies;
 }
 WorksController.$inject = ['worksService'];
