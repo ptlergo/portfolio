c3f08a709ce33db0c140307436547829984ad387
ptlergo <ptlergo@gmail.com>
2017-04-06 04:51:09 -0400

    homeservice init

---
+++ client/js/services/home.service.js
@@ -0,0 +1,13 @@
+/**
+  * @ngdoc method
+  * @name homeService
+  * @methodOf module.homeService
+  * @description
+  * Home Page service for section summaries
+  *
+  * @returns {}
+*/
+function homeService() {
+  return {}
+}
+angular.module('myApp').factory('homeService', homeService);
