2336dc988901e339b6f09c191331627da6ecc02e
ptlergo <ptlergo@gmail.com>
2017-04-06 20:51:38 -0400

    menu sass for hamburger and overlay. navbar bootstrap override. vh used appropiately

--- client/js/controllers/home.controller.js
+++ client/js/controllers/home.controller.js
@@ -11,6 +11,7 @@ function HomeController(homeService) {
   const vm = this;
   vm.main_logo = '../../img/pat-logo.svg';
   vm.homeService = homeService;
+  vm.name = homeService.Content;
   vm.summary = homeService.Content.about.summary;
   vm.templates = [
     homeService.Content.about.templateUrl,
