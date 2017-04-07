44c37b85ac9684e95856de59adf2cb002637f791
ptlergo <ptlergo@gmail.com>
2017-04-07 09:28:43 -0400

    alignmnet of about me page items

--- client/js/controllers/about.controller.js
+++ client/js/controllers/about.controller.js
@@ -12,6 +12,7 @@ function AboutController(aboutService, homeService) {
   const aboutPage = aboutService.Proficiencies;
   const about = homeService.Content.about;
   vm.img_src = about.imgUrl;
+  vm.aboutService = aboutService;
   vm.title = about.title;
   vm.skills = aboutPage.development.skills.list;
   vm.techniquesDev = aboutPage.development.techniques.list;
