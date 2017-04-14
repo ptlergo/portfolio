30a32eaf767146f2fcd03785e0981aaf544f567b
ptlergo <ptlergo@gmail.com>
2017-04-14 01:38:16 -0400

    flexbox alignment for vid

--- client/js/controllers/publications.controller.js
+++ client/js/controllers/publications.controller.js
@@ -7,13 +7,14 @@
   *
   * @returns {}
 */
-function PublicationsController(publicationService) {
+function PublicationsController(publicationService, $sce) {
   const vm = this;
   const publications = publicationService.publications;
   vm.img_src = publications.imgUrl;
   vm.title = publications.title;
   vm.summary = publications.summary;
   vm.links = publications.links;
+  vm.video = $sce.trustAsResourceUrl(publications.links[0].src);
 }
-PublicationsController.$inject = ['publicationService'];
+PublicationsController.$inject = ['publicationService', '$sce'];
 angular.module('myApp').controller('PublicationsController', PublicationsController);
