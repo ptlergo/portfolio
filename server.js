92edf72d4d6c2bfb57c52166c311fe521beaefe2
unknown <ptlergo@gmail.com>
2018-11-10 18:24:02 -0500

    fix server. handles frontend routing better, no 404 errors. uses cors. removed mongoose temporarily

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const morgan = require('morgan');
+// const morgan = require('morgan');
+const cors = require('cors')
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const FRONTEND_PATH = path.join(__dirname, './client');
+const FRONTEND_PATH = path.join(__dirname, '/client');
 // middleware for express app to use
-app.use(cors());
-app.use(bodyParser.json());
+
 // endpoints location
-app.use('/',routes);
+// app.use('/',routes);
 // mount the path to base directory
 app.use('/', express.static(FRONTEND_PATH));
-
+app.use(cors());
+app.use(bodyParser.json());
+// fixes 404 error of frontend routing
+app.get('*', res => {
+    res.sendFile(`${FRONTEND_PATH}/index.html`);
+  })
 // mongodb://<dbuser>:<dbpassword>@ds147073.mlab.com:47073/heroku_7qdrf3jt
 // connect to db instance and the 'issues' collection
-mongoose.connect(CONNECTION_URI);
+// mongoose.connect(CONNECTION_URI);
 
-const connection = mongoose.connection;
-// listen to open of db
-connection.once('open', () => {
-    console.log("mongodb database conenction established successfully");
-});
+// const connection = mongoose.connection;
+// // listen to open of db
+// connection.once('open', () => {
+//     console.log("mongodb database conenction established successfully");
+// });
   
 // app.get('/list', (req, res) => res.send(`welcome to the issue tracker app by Patrick Tunga-Lergo. Listening on Port : ${PORT}`));
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
