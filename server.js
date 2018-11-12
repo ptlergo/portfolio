ff21207f5c5b6022122274c8769ab37ebe204acc
unknown <ptlergo@gmail.com>
2018-11-12 17:41:34 -0500

    eslint and prettier config. 404 redierct update.

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 // const morgan = require('morgan');
-const cors = require('cors')
 
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const FRONTEND_PATH = path.join(__dirname, '/client');
 // middleware for express app to use
-
 // endpoints location
 // app.use('/',routes);
 // mount the path to base directory
@@ -17,18 +17,16 @@ app.use('/', express.static(FRONTEND_PATH));
 app.use(cors());
 app.use(bodyParser.json());
 // fixes 404 error of frontend routing
-app.get('*', res => {
-    res.sendFile(`${FRONTEND_PATH}/index.html`);
-  })
+app.get('*', (req, res) => {
+  res.sendFile(`${FRONTEND_PATH}/index.html`);
+});
 // mongodb://<dbuser>:<dbpassword>@ds147073.mlab.com:47073/heroku_7qdrf3jt
 // connect to db instance and the 'issues' collection
 // mongoose.connect(CONNECTION_URI);
-
 // const connection = mongoose.connection;
 // // listen to open of db
 // connection.once('open', () => {
 //     console.log("mongodb database conenction established successfully");
 // });
-  
 // app.get('/list', (req, res) => res.send(`welcome to the issue tracker app by Patrick Tunga-Lergo. Listening on Port : ${PORT}`));
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
