aed17732ba30877a63367b21edc6d16e054cff00
Patrick <patrick@dothello.io>
2021-12-23 11:23:15 -0500

    dynamic date

--- client/js/controllers/home.controller.js
+++ client/js/controllers/home.controller.js
@@ -12,6 +12,7 @@ function HomeController(homeService) {
   vm.main_logo = homeService.brand;
   vm.homeService = homeService;
   vm.socialLinks = homeService.Contact.social;
+  vm.date = new Date().getFullYear();
   vm.otherLinks = homeService.Contact.other;
   vm.name = homeService.Content;
   vm.summary = homeService.Content.about.summary;
