d57d78f0bdc77f52c7925c57e7866548b12efb77
patrick tunga-lergo <ptlergo@gmail.com>
2026-08-26 15:00:26 -0400

    update gulp, sass legacy code to fix local run errors

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,21 @@ const imageMin = require('gulp-imagemin');
 const sassSource = 'client/stylesheets/sass/**/*.scss';
 const sassDest = './client/stylesheets/css';
 const images = ['client/img/*'];
+
+// Polyfill for Object.fromEntries to fix engine.io crash on Node v12
+if (!Object.fromEntries) {
+  Object.fromEntries = function (entries) {
+    if (!entries || !entries[Symbol.iterator]) {
+      throw new TypeError('Object.fromEntries() requires an iterable object');
+    }
+    const obj = {};
+    for (const [key, value] of entries) {
+      obj[key] = value;
+    }
+    return obj;
+  };
+}
+
 // Static Server run using browsersync
 gulp.task('run', () => {
   browserSync.init({
@@ -27,10 +42,19 @@ gulp.task('sass', () => {
 });
 
 // Default task that watches Sass, html, and reloads browsersync for changes
-gulp.task('default', ['run'], () => {
-  gulp.watch(sassSource, ['sass']);
+// gulp.task('default', ['run'], () => {
+//   gulp.watch(sassSource, ['sass']);
+//   gulp.watch('./client/*.html').on('change', browserSync.reload);
+//   gulp.src(images)
+//     .pipe(imageMin())
+//     .pipe(gulp.dest('client/img'));
+// });
+gulp.task('default', gulp.series('run', () => {
+  gulp.watch(sassSource, gulp.series('sass'));
   gulp.watch('./client/*.html').on('change', browserSync.reload);
-  gulp.src(images)
+  
+  // Note: Added a 'return' here so Gulp knows exactly when this function finishes
+  return gulp.src(images)
     .pipe(imageMin())
     .pipe(gulp.dest('client/img'));
-});
+}));
--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/', (req, res) => {
+    res.send('Server is working!');
+});
+
 // Logger styling
 app.use(morgan('dev'));
 
